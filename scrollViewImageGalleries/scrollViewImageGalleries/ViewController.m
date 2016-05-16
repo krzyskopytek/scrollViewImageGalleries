da42b9b0db6c791373247caafe71ca84fac5054f
krzyskopytek <krzys.kopytek@hotmail.com>
2016-05-16 13:35:03 -0400

    part one

--- scrollViewImageGalleries/scrollViewImageGalleries/ViewController.m
+++ scrollViewImageGalleries/scrollViewImageGalleries/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
 
 @end
 
@@ -16,12 +16,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // setup programmatically ... storybord su#$ :P
+    [self setupMyScroll];
+    
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+// ====================================================================================================
+-(void) setupMyScroll{
+    
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectInset(self.view.frame, 20, 20)];
+    
+    //3 times wide because 3 images
+    UIView *myImageContainer = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 3*CGRectGetWidth(scrollView.frame),  CGRectGetHeight(scrollView.frame))];
+    
+    // adding 3 images 320 x 480 ... setting frames manualy (only 3 images :P) ... learning curve
+    UIImageView *firstImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"puppy1.jpg"]];
+    firstImage.frame = CGRectMake(0, 0, CGRectGetWidth(scrollView.frame), CGRectGetHeight(scrollView.frame));
+    firstImage.contentMode = UIViewContentModeScaleAspectFit;
+    
+    UIImageView *secondImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"puppy2.jpg"]];
+    secondImage.frame = CGRectMake(CGRectGetWidth(scrollView.frame), 0, CGRectGetWidth(scrollView.frame), CGRectGetHeight(scrollView.frame));
+    secondImage.contentMode = UIViewContentModeScaleAspectFit;
+    
+    UIImageView *thirdImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"puppy3.jpg"]];
+    thirdImage.frame = CGRectMake(2*CGRectGetWidth(scrollView.frame), 0, CGRectGetWidth(scrollView.frame), CGRectGetHeight(scrollView.frame));
+    thirdImage.contentMode = UIViewContentModeScaleAspectFit;
+    
+    //adding 3 ImageViews to one containter
+    [myImageContainer addSubview:firstImage];
+    [myImageContainer addSubview:secondImage];
+    [myImageContainer addSubview:thirdImage];
+    
+    //adding container to scroll
+    [scrollView addSubview:myImageContainer];
+    
+    //seting scroll params
+    scrollView.contentSize = CGSizeMake(CGRectGetWidth(myImageContainer.frame), CGRectGetHeight(scrollView.frame));
+    scrollView.pagingEnabled = YES;
+    scrollView.delegate = self;
+    [self.view addSubview:scrollView];
+    
+    
 }
+
 
 @end
